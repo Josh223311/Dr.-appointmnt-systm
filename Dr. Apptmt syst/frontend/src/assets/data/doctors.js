a92825cd044bd0560ce76ca64be368b75521b9eb
Jyosthna V <135004424+Josh223311@users.noreply.github.com>
2023-10-09 14:03:05 +0530

    Add files via upload

---
+++ Dr. Apptmt syst/frontend/src/assets/data/doctors.js
@@ -0,0 +1,36 @@
+import doctorImg01 from "../images/doctor-img01.png";
+import doctorImg02 from "../images/doctor-img02.png";
+import doctorImg03 from "../images/doctor-img03.png";
+
+export const doctors = [
+  {
+    id: "01",
+    name: "Dr. Sunitha",
+    specialty: "Surgeon",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg01,
+    totalPatients: 1500,
+    hospital: "KIMS , Hyderabad",
+  },
+  {
+    id: "02",
+    name: "Dr. Vaanindhar",
+    specialty: "Neurologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg02,
+    totalPatients: 1500,
+    hospital: "Rainbow children Hospital, Banjarahills",
+  },
+  {
+    id: "03",
+    name: "Dr. Amul Gandhi",
+    specialty: "Dermatologist",
+    avgRating: 4.8,
+    totalRating: 272,
+    photo: doctorImg03,
+    totalPatients: 1500,
+    hospital: "Yashoda Hospital, Hyd",
+  },
+];
